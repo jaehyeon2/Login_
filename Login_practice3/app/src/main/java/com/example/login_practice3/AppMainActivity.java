0f2066d993d94e47b81a96959aa62e98742592d3
Kang JaeHyeon <59330378+jaehyeon2@users.noreply.github.com>
2020-04-28 15:17:20 +0900

    initial commit

---
+++ Login_practice3/app/src/main/java/com/example/login_practice3/AppMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.login_practice3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppMainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_complete);
+
+    }
+}
